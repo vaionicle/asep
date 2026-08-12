e0201d3e7b6f9b4c3719983a9e78b7839f40539e
Vasileios Tsaknis <tsaknis_vasilios@hotmail.com>
2026-08-12 16:23:32 +0300

    Setting correct path for import

--- scripts/2ge_2026.sh
+++ scripts/2ge_2026.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./tmp/anaplirotes/2GE_2026_PROSORINOI
+cd ./tmp/pinakes/2GE_2026_PROSORINOI
 
 for file in 1_*.xls; do
     [ -f "$file" ] || continue
@@ -11,5 +11,5 @@ for file in 1_*.xls; do
         spec=$(printf '%s' "$file" | grep -o 'ΠΕ[0-9]\+' | head -n1)
     fi
 
-    echo make import.qualifications SPEC=\"${spec}\" FILE=\"anaplirotes/2GE_2026_PROSORINOI/$file\"
+    echo make import.qualifications SPEC=\"${spec}\" FILE=\"pinakes/2GE_2026_PROSORINOI/$file\"
 done
